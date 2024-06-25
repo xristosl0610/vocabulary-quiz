c08ca4c0deecbf892234cb7f3f7edafe41718696
Christos Lathourakis <christos.lathourakis@tno.nl>
2024-06-25 13:52:25 +0200

    fix testing bug

--- tests/test_quiz.py
+++ tests/test_quiz.py
@@ -1,11 +1,14 @@
 import unittest
+import os
 from vocab_quiz.quiz import read_csv
 
 
 class TestVocabQuiz(unittest.TestCase):
 
     def test_read_csv(self):
-        vocab_list = read_csv('data/Dutch_Vocabulary.csv')
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        csv_file_path = os.path.join(current_dir, '../data/Dutch_Vocabulary.csv')
+        vocab_list = read_csv(csv_file_path)
         self.assertIsInstance(vocab_list, list)
         self.assertGreater(len(vocab_list), 0)
         self.assertIn('Dutch', vocab_list[0])
