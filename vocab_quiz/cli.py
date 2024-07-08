5bc9bca3c7ecdfb9b1ab10b387972d70811f50cd
Christos Lathourakis <christos.lathourakis@tno.nl>
2024-07-08 15:22:43 +0200

    Added a more user-friendly CLI version of adding words, one by one

--- vocab_quiz/cli.py
+++ vocab_quiz/cli.py
@@ -13,20 +13,30 @@ def main() -> None:
     parser.add_argument("--direction", type=str, choices=['nl_en', 'en_nl'],
                         default='nl_en', help="Direction of the quiz")
 
-    parser.add_argument("--add_word", action="store_true", help="Add a new word to the vocabulary")
-    parser.add_argument("--dutch_word", type=str, help="Dutch word to add")
-    parser.add_argument("--english_word", type=str, help="English word to add")
-    parser.add_argument("--additional_info", type=str, help="Additional info for the new word")
+    parser.add_argument("--interactive", action="store_true", help="Interactive mode for adding new words")
 
     args: Namespace = parser.parse_args()
 
     vocab_df: pd.DataFrame = read_csv(args.csv_file)
 
-    if args.add_word:
-        if not args.dutch_word or not args.english_word or not args.additional_info:
-            print("To add a word, you must provide the Dutch word, English word, and additional info.")
-            return
-        vocab_df = add_word(vocab_df, args.dutch_word, args.english_word, args.additional_info)
+    if args.interactive:
+        print("Welcome to the Vocabulary Quiz CLI")
+        print("Let's add some new words for future practice")
+
+        while True:
+            dutch_word = input("Dutch word: ").strip()
+            english_word = input("English translation: ").strip()
+            additional_info = input("Additional Info: ").strip()
+
+            if ((vocab_df['Dutch'] == dutch_word) | (vocab_df['English'] == english_word)).any():
+                print(f"The word '{dutch_word}' or its translation '{english_word}' already exists.")
+            else:
+                vocab_df = add_word(vocab_df, dutch_word, english_word, additional_info)
+                print(f"{dutch_word} - {english_word} - {additional_info} -- has been added successfully")
+
+            more_words = input("More words to add? (y/n): ").strip().lower()
+            if more_words not in ['y', 'yes']:
+                break
     else:
         quiz(vocab_df, args.num_words, args.direction)
 
