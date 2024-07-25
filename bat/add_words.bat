87ab488f6e7ee04de345405831ec0fe3ebd5f784
Christos Lathourakis <christos.lathourakis@tno.nl>
2024-07-25 21:38:29 +0200

    Updated requirements.txt and setup.py, and fixed the batch file paths

--- bat/add_words.bat
+++ bat/add_words.bat
@@ -3,5 +3,5 @@ REM Change directory to the directory of the batch file
 cd /d "C:\Users\lathourakisc\Projects\Dutch\vocabulary_quiz"
 
 REM Run Python scripts using relative paths
-cmd /c ".\.venv\Scripts\python.exe -m vocab_quiz.cli data\Dutch_Vocabulary.csv --interactive"
+cmd /c ".\venv\Scripts\python.exe -m vocab_quiz.cli data\Dutch_Vocabulary.csv --interactive"
 pause
