c0d043c3d49242156c6b945d7abf0e05a6829b94
Christos Lathourakis <christos.lathourakis@tno.nl>
2024-07-25 21:38:29 +0200

    Updated requirements.txt and setup.py, and fixed the batch file paths

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="vocab_quiz",
     version="0.1.0",
-    packages=find_packages(),
-    install_requires=[
-        # List your project's dependencies here.
-    ],
+    description='A fun way to study vocabulary, among Dutch and English',
+    author='Christos Lathourakis',
+    author_email='xristosl0610@gmail.com',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=requirements,
     entry_points={
         "console_scripts": [
             "vocab_quiz = vocab_quiz.cli:main",
         ],
     },
+    python_requires='>=3.9'
 )
