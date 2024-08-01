644434aa9122138ea9d06fc40a17473c88a83659
Christos Lathourakis <christos.lathourakis@tno.nl>
2024-08-01 11:01:50 +0200

    use of the local "%~dp0" variable

--- bat/run_vocab_quiz.bat
+++ bat/run_vocab_quiz.bat
@@ -4,7 +4,7 @@ setlocal
 set "BASE_DIR=%~dp0"
 
 REM Change to the base directory
-cd /d "%BASE_DIR%" && cd /d ..
+cd /d "%~dp0.."
 
 REM Run Python scripts using relative paths
 cmd /c ".\venv\Scripts\python.exe -m vocab_quiz.cli data\Dutch_Vocabulary.csv --num_words 50 --direction nl_en && .\venv\Scripts\python.exe -m vocab_quiz.cli data\Dutch_Vocabulary.csv --num_words 50 --direction en_nl"
