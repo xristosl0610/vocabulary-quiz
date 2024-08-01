8bdaf9326220e951ac2148f6bf2d5ba075823219
Christos Lathourakis <christos.lathourakis@tno.nl>
2024-08-01 10:48:53 +0200

    Made the paths relative

--- bat/add_words.bat
+++ bat/add_words.bat
@@ -1,6 +1,10 @@
 @echo off
-REM Change directory to the directory of the batch file
-cd /d "C:\Users\lathourakisc\Projects\Dutch\vocabulary_quiz"
+REM Get the directory of the batch file
+setlocal
+set "BASE_DIR=%~dp0"
+
+REM Change to the base directory
+cd /d "%BASE_DIR%" && cd /d ..
 
 REM Run Python scripts using relative paths
 cmd /c ".\venv\Scripts\python.exe -m vocab_quiz.cli data\Dutch_Vocabulary.csv --interactive"
--- bat/run_vocab_quiz.bat
+++ bat/run_vocab_quiz.bat
@@ -1,6 +1,10 @@
 @echo off
-REM Change directory to the directory of the batch file
-cd /d "C:\Users\lathourakisc\Projects\Dutch\vocabulary_quiz"
+REM Get the directory of the batch file
+setlocal
+set "BASE_DIR=%~dp0"
+
+REM Change to the base directory
+cd /d "%BASE_DIR%" && cd /d ..
 
 REM Run Python scripts using relative paths
 cmd /c ".\venv\Scripts\python.exe -m vocab_quiz.cli data\Dutch_Vocabulary.csv --num_words 50 --direction nl_en && .\venv\Scripts\python.exe -m vocab_quiz.cli data\Dutch_Vocabulary.csv --num_words 50 --direction en_nl"
