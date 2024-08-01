a8984c4ac5903cf11c3d8706a54b7a467f968cef
Christos Lathourakis <53656840+xristosl0610@users.noreply.github.com>
2024-08-01 10:56:49 +0200

    Update bat/add_words.bat
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- bat/add_words.bat
+++ bat/add_words.bat
@@ -4,7 +4,7 @@ setlocal
 set "BASE_DIR=%~dp0"
 
 REM Change to the base directory
-cd /d "%BASE_DIR%" && cd /d ..
+cd /d "%~dp0.."
 
 REM Run Python scripts using relative paths
 cmd /c ".\venv\Scripts\python.exe -m vocab_quiz.cli data\Dutch_Vocabulary.csv --interactive"
