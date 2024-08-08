31d720d1fe4aaa4644cdc0c3106c20b788fa9967
Christos Lathourakis <xristosl0610@gmail.com>
2024-08-08 21:20:16 +0200

    Made paths in batch files relative

--- bat/short_quiz.bat
+++ bat/short_quiz.bat
@@ -1,7 +1,9 @@
 @echo off
-REM Change to the base directory
-cd /d "%~dp0.."
+REM Change to the project root directory
+cd /d "%~dp0\.."
 
 REM Run Python scripts using relative paths
-cmd /c ".\venv\Scripts\python.exe -m vocab_quiz.cli data\Dutch_Vocabulary.csv --num_words 10 --direction nl_en && .\venv\Scripts\python.exe -m vocab_quiz.cli data\Dutch_Vocabulary.csv --num_words 10 --direction en_nl"
+".venv\Scripts\python.exe" -m vocab_quiz.cli data\Dutch_Vocabulary.csv --num_words 10 --direction nl_en
+".venv\Scripts\python.exe" -m vocab_quiz.cli data\Dutch_Vocabulary.csv --num_words 10 --direction en_nl
+
 pause
