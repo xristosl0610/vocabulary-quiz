519b55c7b6bf8f04004c58f5c16f1f57ab2b22dd
Christos Lathourakis <christos.lathourakis@tno.nl>
2024-08-09 09:42:39 +0200

    Made ALL path work in both venv and .venv names of the python executable

--- bat/add_words.bat
+++ bat/add_words.bat
@@ -2,6 +2,21 @@
 REM Change to the base directory
 cd /d "%~dp0.."
 
+REM Check if ".venv" exists
+if exist ".venv\Scripts\python.exe" (
+    set VENV_PATH=".venv\Scripts\python.exe"
+) else (
+    REM Check if "venv" exists
+    if exist "venv\Scripts\python.exe" (
+        set VENV_PATH="venv\Scripts\python.exe"
+    ) else (
+        echo No virtual environment found. Make sure either ".venv" or "venv" is set up.
+        pause
+        exit /b
+    )
+)
+
 REM Run Python scripts using relative paths
-cmd /c ".\venv\Scripts\python.exe -m vocab_quiz.cli data\Dutch_Vocabulary.csv --interactive"
+%VENV_PATH% -m vocab_quiz.cli data\Dutch_Vocabulary.csv --interactive
+
 pause
--- bat/run_vocab_quiz.bat
+++ bat/run_vocab_quiz.bat
@@ -2,6 +2,22 @@
 REM Change to the base directory
 cd /d "%~dp0.."
 
-REM Run Python scripts using relative paths
-cmd /c ".\venv\Scripts\python.exe -m vocab_quiz.cli data\Dutch_Vocabulary.csv --num_words 50 --direction nl_en && .\venv\Scripts\python.exe -m vocab_quiz.cli data\Dutch_Vocabulary.csv --num_words 50 --direction en_nl"
+REM Check if ".venv" exists
+if exist ".venv\Scripts\python.exe" (
+    set VENV_PATH=".venv\Scripts\python.exe"
+) else (
+    REM Check if "venv" exists
+    if exist "venv\Scripts\python.exe" (
+        set VENV_PATH="venv\Scripts\python.exe"
+    ) else (
+        echo No virtual environment found. Make sure either ".venv" or "venv" is set up.
+        pause
+        exit /b
+    )
+)
+
+REM Run Python scripts using the determined virtual environment
+%VENV_PATH% -m vocab_quiz.cli data\Dutch_Vocabulary.csv --num_words 50 --direction nl_en
+%VENV_PATH% -m vocab_quiz.cli data\Dutch_Vocabulary.csv --num_words 50 --direction en_nl
+
 pause
